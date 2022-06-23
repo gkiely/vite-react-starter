80595fa3698b00cbd3d546c1a2057d50ea351fd5
Grant Kiely <grant.kiely@gmail.com>
2022-06-23 09:47:11 -0500

    updated package.json and get-latest script

--- scripts/get-latest.ts
+++ scripts/get-latest.ts
@@ -1,10 +1,10 @@
 const package = require('../package.json');
 
-const deps = Object.keys(
-  {
+const deps =
+  Object.keys({
     ...package.dependencies,
     ...package.devDependencies,
-  }
-).join('@latest ') + '@latest';
+  }).join('@latest ') + '@latest';
 
+// Output to shell
 console.log(deps);
