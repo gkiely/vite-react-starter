16f6c4f39a6b605de34b5768cca9a52ec6dc7f66
Grant Kiely <grant.kiely@gmail.com>
2022-07-08 16:14:30 -0500

    removed fragment test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,7 @@ import App from './App';
 
 describe('App', () => {
   it('should render', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
-
+    render(<App />);
     expect(screen.getByText('Hello Vite + React!')).toBeInTheDocument();
     const button = screen.getByRole('button');
     fireEvent.click(button);
