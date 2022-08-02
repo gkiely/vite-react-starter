fb4805ded1941ca671f7791e4ed7a3339af24a1e
Grant Kiely <grant.kiely@gmail.com>
2022-08-01 20:42:18 -0500

    Clean up

--- src/app/routes/server.ts
+++ src/app/routes/server.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono';
 import { bodyParse } from 'hono/body-parse';
-import { partialStore, Store } from 'server/schemas';
+import { partialStore, requests, requestSchema, Store } from 'server/schemas';
 import { delay } from 'utils';
 import { z } from 'zod';
 
@@ -11,28 +11,6 @@ export const initialState: Store = {
   loading: '',
 };
 
-/// TODO: hopefully there's a way to get these types via zod
-// so we can define them inline
-const requestMap = {
-  '/api/count': {
-    POST: z.object({ count: z.number() }),
-  },
-  '/api/post': {
-    POST: z.object({ title: z.string() }),
-    DELETE: z.object({ id: z.string() }),
-  },
-};
-
-// Used for requestParse
-const requestSchema = z.union([
-  requestMap['/api/count'].POST,
-  requestMap['/api/post'].POST,
-  requestMap['/api/post'].DELETE,
-]);
-
-export const requestParse = (request: z.infer<typeof requestSchema>) =>
-  requestSchema.parse(request);
-
 const actionSchema = z.union([
   z.object({
     path: z.literal('/api/count'),
@@ -40,7 +18,7 @@ const actionSchema = z.union([
     options: z
       .object({
         method: z.literal('POST'),
-        body: requestMap['/api/count'].POST,
+        body: requests['/api/count'].POST,
       })
       .optional(),
   }),
@@ -50,7 +28,7 @@ const actionSchema = z.union([
     options: z
       .object({
         method: z.literal('POST'),
-        body: requestMap['/api/post'].POST,
+        body: requests['/api/post'].POST,
       })
       .optional(),
   }),
@@ -60,7 +38,7 @@ const actionSchema = z.union([
     options: z
       .object({
         method: z.literal('DELETE'),
-        body: requestMap['/api/post'].DELETE,
+        body: requests['/api/post'].DELETE,
       })
       .optional(),
   }),
@@ -79,7 +57,7 @@ export const store: Store = {
 };
 /* c8 ignore start */
 export const app = new Hono();
-type APIPath = keyof typeof requestMap;
+type APIPath = keyof typeof requests;
 
 app.post<APIPath>('/api/count', bodyParse(), async (c) => {
   const { req } = c;
@@ -93,7 +71,7 @@ app.post<APIPath>('/api/count', bodyParse(), async (c) => {
 
 app.post<APIPath>('/api/post', bodyParse(), (c) => {
   const { req } = c;
-  const body = requestMap['/api/post'].POST.parse(req.parsedBody);
+  const body = requests['/api/post'].POST.parse(req.parsedBody);
   const post = {
     id: `post-${store.posts.length + 1}`,
     title: body.title,
@@ -104,7 +82,7 @@ app.post<APIPath>('/api/post', bodyParse(), (c) => {
 
 app.delete<APIPath>('/api/post', bodyParse(), (c) => {
   const { req } = c;
-  const post = requestMap['/api/post'].DELETE.parse(req.parsedBody);
+  const post = requests['/api/post'].DELETE.parse(req.parsedBody);
   store.posts = store.posts.filter((p) => p.id !== post.id);
   return c.json(store);
 });
