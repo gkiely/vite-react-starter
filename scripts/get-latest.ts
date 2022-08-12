f3f4e3d2f53d7888371180d3b51d319d1eeb6a77
Grant Kiely <grant.kiely@gmail.com>
2022-08-12 15:09:24 -0500

    updated deps and get-latest script to ignore typescript

--- scripts/get-latest.ts
+++ scripts/get-latest.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prefer-template */
 import packageJSON from '../package.json';
 
-const deps =
+const deps = (
   Object.keys({
     ...packageJSON.dependencies,
     ...packageJSON.devDependencies,
-  }).join('@latest ') + '@latest';
+  }).join('@latest ') + '@latest'
+).replace('typescript@latest ', '');
 
 // Output to shell
 console.log(deps);
