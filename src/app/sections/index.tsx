7e68ce4a98d5488a34849d5f8e2de11dfa6e9ec3
Grant Kiely <grant.kiely@gmail.com>
2022-08-14 17:25:05 -0500

    testing fix

--- src/app/sections/index.tsx
+++ src/app/sections/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import * as styles from './sections.css';
 
 type Props = PropsWithChildren;
-
+/* c8 ignore start */
 export const Column = ({ children }: Props) => {
   return <section className={styles.column}>{children}</section>;
 };
@@ -10,3 +10,4 @@ export const Column = ({ children }: Props) => {
 export const Row = ({ children }: Props) => {
   return <section className={styles.row}>{children}</section>;
 };
+/* c8 ignore stop */
