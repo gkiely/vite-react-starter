af4cd172e032c67e224f169181d2fd44aa8547bf
Grant Kiely <grant.kiely@gmail.com>
2022-09-04 15:52:19 -0500

    updated vitest

--- src/app/routes/paths.ts
+++ src/app/routes/paths.ts
@@ -1,7 +1,9 @@
 import { toEnum } from 'utils';
 
+/* c8 ignore start */
 export const paths = ['', '/', '/second', '/third'] as const;
 
 export type Path = typeof paths[number];
 
 export const pathsEnum = toEnum(paths);
+/* c8 ignore stop */
