478918dbe2f09e1266faa94534285928ff3e79db
Grant Kiely <grant.kiely@gmail.com>
2022-09-09 16:08:34 -0500

    Added vitest error handling for consoles

--- setup.vitest.ts
+++ setup.vitest.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { configure } from '@testing-library/react';
-import { vi } from 'vitest';
+import { expect, vi } from 'vitest';
 
 // Disable runtime css
 import '@vanilla-extract/css/disableRuntimeStyles';
@@ -30,3 +30,24 @@ vi.stubGlobal('fetch', () => ({
 vi.stubGlobal('location', {
   pathname: '/',
 });
+
+// When pushing to production, fail if console logs
+if (process.env.VITEST_MODE === 'WATCH' || process.env.npm_lifecycle_event !== 'coverage') {
+  const { log } = console;
+  const reportError = (msg: string, type: 'log' | 'warn' | 'error') => {
+    log(msg);
+    const err = new Error(`console.${type} was called in test`);
+    expect.fail(
+      (err.stack as string)
+        .replace(/(.+setup\.vitest\.ts.+\n+)/g, '')
+        .replace(/.+file:\/\/\/.+\n/g, '')
+        .replace(/.+\/node_modules\/.+\n?/g, '')
+    );
+  };
+
+  vi.stubGlobal('console', {
+    log: (msg: string) => reportError(msg, 'log'),
+    warn: (msg: string) => reportError(msg, 'warn'),
+    error: (msg: string) => reportError(msg, 'error'),
+  });
+}
