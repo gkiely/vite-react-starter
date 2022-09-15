c7c257cd961f616b7bf615b317334c72ea21dd23
Grant Kiely <grant.kiely@gmail.com>
2022-09-15 12:52:57 -0500

    updates

--- src/app/utils/index.tsx
+++ src/app/utils/index.tsx
@@ -2,7 +2,11 @@ import { Context, Next } from 'hono';
 import type { EventObject } from 'xstate';
 
 /* c8 ignore start */
-export function assertType<T>(value: unknown): asserts value is T {}
+export function assertType<T>(value: unknown): asserts value is T {
+  if (value === undefined) {
+    throw new Error('value must be defined');
+  }
+}
 
 export function assertEventType<TE extends EventObject, TType extends TE['type']>(
   event: TE,
